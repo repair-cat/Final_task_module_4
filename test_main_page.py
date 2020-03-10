a5f0ae3879e9f5755c64c114d247448e12c503cd
repair-cat <simf61@yandex.ru>
2020-03-10 11:14:03 +0500

    go_to_login_page function was output as a separated func

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,7 @@ def go_to_login_page(browser):
     login_link.click()
 
 
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/" 
+    browser.get(link) 
+    go_to_login_page(browser) 

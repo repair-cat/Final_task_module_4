2bc97b898238e9412e85d8267ecc5065f01a0fd6
repair-cat <simf61@yandex.ru>
2020-03-11 14:28:17 +0500

    added method: should_be_login_link

---
+++ pages/main_page.py
@@ -0,0 +1,17 @@
+"""реализуем Page Object, который будет связан с главной страницей интернет-магазина."""
+
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage): 
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+
+
+
+

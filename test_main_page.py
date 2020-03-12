2785331205c0b0c505fd69a29c699292e1ea8519
repair-cat <simf61@yandex.ru>
2020-03-12 16:35:36 +0500

    3 tests of login page included in test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -9,6 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                         # открываем страницу
     page.go_to_login_page()             # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)    # реализация трех тестов из login_page.py
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -16,8 +18,3 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-
-def test_correct_login_url(browser):            # реализация трех тестов из login_page.py
-    page = LoginPage(browser, login_link)
-    page.open()
-    page.should_be_login_page()

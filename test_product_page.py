b0e6c1afd762cd7e156ed526c97ff9a562f6305c
repair-cat <simf61@yandex.ru>
2020-03-16 07:49:04 +0500

    temporarily skiped 4 tests and added 2 tests

--- test_product_page.py
+++ test_product_page.py
@@ -15,9 +15,53 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
                         )
 
+
+@pytest.mark.skip       # временно для пропуска теста
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.product_should_be_add_to_basket()      # находим элемент корзины и кликаем на него, вводим ответ на alert
-    page.product_added_to_basket()              # сравнение имени товара и имени товара в сообщении
+    page.product_name_like_in_basket()          # сравнение имени товара и имени товара в сообщении
     page.product_price_is_equal_to_basket()     # сравнение цены товара и корзины               
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    # тест на проверку отсутсвия сообщения о добавлении в корзину после нажатия на кнопку "Добавить в корзину"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
+    page = ProductPage(browser, link)
+    page.open()
+    page.product_should_be_add_to_basket()
+    page.should_not_be_success_message()         
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):
+    # тест на проверку отсутсвия сообщения о добавлении в корзину сразу после открытия страницы с товаром
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()        
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser): 
+    # тест на проверку об исчезновении сообщения о добавлении товара в корзину после нажатия на кнопку "Добавить в корзину"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
+    page = ProductPage(browser, link)
+    page.open()
+    page.product_should_be_add_to_basket()
+    page.should_disappear_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    # ссылку на логин видно со страницы продукта
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    # пользователь пожет зайти в логин со страницы продукта
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()

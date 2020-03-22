1fac71aaa813928466b232c34f91b0049c708afb
repair-cat <simf61@yandex.ru>
2020-03-22 17:31:20 +0300

    a link for all tests and a class for 2 tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,68 +1,67 @@
 """тест-кейсы для страниц товаров"""
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import pytest
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
-                        )
+@pytest.mark.user_to_basket
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = 'Galagramma2'
+        page = LoginPage(browser, link)
+        page.open()
+        page.go_to_login_page()      
+        page.register_new_user(email, password)
 
 
-@pytest.mark.skip       # временно для пропуска теста
-def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
-    page.product_should_be_add_to_basket()      # находим элемент корзины и кликаем на него, вводим ответ на alert
-    page.product_name_like_in_basket()          # сравнение имени товара и имени товара в сообщении
-    page.product_price_is_equal_to_basket()     # сравнение цены товара и корзины               
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.product_should_be_add_to_basket()      # находим элемент корзины и кликаем на него, вводим ответ на alert
+        page.product_name_like_in_basket()          # сравнение имени товара и имени товара в сообщении
+        page.product_price_is_equal_to_basket()     # сравнение цены товара и корзины               
 
-@pytest.mark.skip
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+
+    def test_guest_cant_see_success_message(self, browser):
+        # тест на проверку отсутсвия сообщения о добавлении в корзину сразу после открытия страницы с товаром
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+
+def test_user_cant_see_success_message_after_adding_product_to_basket(browser):
     # тест на проверку отсутсвия сообщения о добавлении в корзину после нажатия на кнопку "Добавить в корзину"
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
     page = ProductPage(browser, link)
     page.open()
     page.product_should_be_add_to_basket()
     page.should_not_be_success_message()         
+        
 
-@pytest.mark.skip
-def test_guest_cant_see_success_message(browser):
-    # тест на проверку отсутсвия сообщения о добавлении в корзину сразу после открытия страницы с товаром
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_not_be_success_message()        
-
-@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser): 
     # тест на проверку об исчезновении сообщения о добавлении товара в корзину после нажатия на кнопку "Добавить в корзину"
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
     page = ProductPage(browser, link)
     page.open()
     page.product_should_be_add_to_basket()
     page.should_disappear_success_message()
 
-@pytest.mark.skip
+
 def test_guest_should_see_login_link_on_product_page(browser):
     # ссылку на логин видно со страницы продукта
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.skip
+
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    # пользователь пожет зайти в логин со страницы продукта
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    # пользователь может зайти в логин со страницы продукта
+    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -70,7 +69,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     # пользователь может зайти в корзину со страницы продукта
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()                                 # открываем старницу товара
     page.go_to_basket()                         # переходим в корзину

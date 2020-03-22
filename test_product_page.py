c5889f5f430ac6b56ee8e75c5205a57dc2ebbb31
repair-cat <simf61@yandex.ru>
2020-03-22 18:20:31 +0300

    added necessary test and skipped 2 tests

--- test_product_page.py
+++ test_product_page.py
@@ -20,6 +20,7 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(email, password)
 
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
@@ -35,14 +36,25 @@ class TestUserAddToBasketFromProductPage():
         page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
+def test_user_can_add_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.product_should_be_add_to_basket()      
+    page.product_name_like_in_basket()          
+    page.product_price_is_equal_to_basket()     
+
+
+@pytest.mark.skip
 def test_user_cant_see_success_message_after_adding_product_to_basket(browser):
     # тест на проверку отсутсвия сообщения о добавлении в корзину после нажатия на кнопку "Добавить в корзину"
     page = ProductPage(browser, link)
     page.open()
     page.product_should_be_add_to_basket()
     page.should_not_be_success_message()         
+ 
         
-
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser): 
     # тест на проверку об исчезновении сообщения о добавлении товара в корзину после нажатия на кнопку "Добавить в корзину"
     page = ProductPage(browser, link)
@@ -53,23 +65,22 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 def test_guest_should_see_login_link_on_product_page(browser):
     # ссылку на логин видно со страницы продукта
-    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     # пользователь может зайти в логин со страницы продукта
-    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     # пользователь может зайти в корзину со страницы продукта
-    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()                                 # открываем старницу товара
     page.go_to_basket()                         # переходим в корзину

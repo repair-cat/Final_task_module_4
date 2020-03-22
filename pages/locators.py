bec33b66039f6243aded65f275770ae339fc960d
repair-cat <simf61@yandex.ru>
2020-03-22 17:23:45 +0300

    locator for authorized user

--- pages/locators.py
+++ pages/locators.py
@@ -3,13 +3,14 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    pass
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+    EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    PASS_1 = (By.CSS_SELECTOR, '#id_registration-password1')
+    PASS_2 = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTRATION_BUTTON = (By.NAME, 'registration_submit')
+    USER_IS_REGISTERED = (By.CSS_SELECTOR, '.wicon')    
 
 
 class ProductPageLocators():
@@ -22,9 +23,10 @@ class ProductPageLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")                             # пользователь залогинен
 
 
 class BasketPageLocators():
-    LOOK_BASKET = (By.XPATH, '//span[@class="btn-group"]')          # ссылка "Посмотреть корзину"
-    BASKET_HAVE_PRODUCT = (By.CSS_SELECTOR, '.col-sm-6.h3')                         # есть товары в корзине
-    BASKET_IS_EMPTY = (By.XPATH, '//p[contains(text(), "Your basket is empty")]')     # заголовок - Ваша корзина пуста
+    LOOK_BASKET = (By.XPATH, '//span[@class="btn-group"]')                  # ссылка "Посмотреть корзину"
+    BASKET_HAVE_PRODUCT = (By.CSS_SELECTOR, '.col-sm-6.h3')                             # есть товары в корзине
+    BASKET_IS_EMPTY = (By.XPATH, '//p[contains(text(), "Your basket is empty")]')       # заголовок - Ваша корзина пуста
